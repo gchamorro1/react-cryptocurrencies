76fa0faf249d0b1f0e662eb0544441c2cf57499d
gaston03 <gaastonchamorro@gmail.com>
2018-10-12 20:49:48 -0300

    v2

--- src/components/BoxTabla/index.js
+++ src/components/BoxTabla/index.js
@@ -2,6 +2,7 @@ import React, { Component } from'react'
 import { Button } from 'react-bootstrap'
 import { observer, inject } from 'mobx-react'
 import api from '../../api'
+import './style.css'
 
 @inject('store')
 @observer
@@ -12,7 +13,8 @@ class BoxTarea extends Component {
         this.state = {
             cryptos: [],
             page: 0,
-            limit: 10
+            limit: 10,
+            order: 'market'
         }  
     }
 
@@ -50,26 +52,62 @@ class BoxTarea extends Component {
         routing.push(`/boxdetails/${item.rank}`)   
     }
 
-    // onSort(items, sortKey){
-    //     items.sort((a,b) => a[sortKey].localeCompare(b[sortKey]))
-    //     this.setState({items})
-    // }
+    ordenarNombre() {
+        let self = this
+        let cryptos = self.state.cryptos
+        if (self.state.order === 'name') {
+            cryptos.sort(function(a, b) {
+                if (a.name > b.name) {
+                    return -1;
+                } else if (a.name < b.name) {
+                    return 1;
+                }
+                return 0;
+            })
+            self.setState({cryptos: cryptos, order: 'nameDes'})
+        } else {
+            cryptos.sort(function(a, b) {
+                if (a.name < b.name) {
+                    return -1;
+                } else if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            })
+            self.setState({cryptos: cryptos, order: 'name'})
+        }        
+    }
 
-    // ordenar(items){
-    //     items.sort(function (a, b) {
-    //         if (a > b) {
-    //           return 1;
-    //         }
-    //         if (a < b) {
-    //           return -1;
-    //         }
-    //         // a must be equal to b
-    //         return 0;
-    //     });
-    //     console.log(items)
-    // }
-    ordenar(items){
-    items.sort(function(a,b){return a - b;});
+    ordenarMarquet() {
+        let cryptos = this.state.cryptos
+        if (this.state.order === 'market') {
+            cryptos.sort(function(a, b) {
+                if (a.quotes.BTC.market_cap > b.quotes.BTC.market_cap) {
+                    return -1;
+                } else if (a.quotes.BTC.market_cap < b.quotes.BTC.market_cap) {
+                    return 1;
+                }
+                return 0;
+            })
+            this.setState({cryptos: cryptos, order: 'marketDes'})
+        } else {
+            cryptos.sort(function(a, b) {
+                if (a.quotes.BTC.market_cap < b.quotes.BTC.market_cap) {
+                    return -1;
+                } else if (a.quotes.BTC.market_cap > b.quotes.BTC.market_cap) {
+                    return 1;
+                }
+                return 0;
+            })
+            this.setState({cryptos: cryptos, order: 'market'})
+        }        
+    }
+
+    itemChange(item) {
+        if(item.quotes.USD.percent_change_24h >= 0){
+            return {color: "green"}
+        }
+        return {color: "red"} 
     }
 
     render() {
@@ -77,47 +115,52 @@ class BoxTarea extends Component {
         let items = self.state.cryptos.map(function(item) {
             return  (
                 <tr style={{cursor: 'pointer'}} key={item.id}  onClick={self.goDetails.bind(self,item)}>
-                    <th>
+                    <th className= "item-rank">
                         {item.rank}  
                     </th>
-                    <td>        
-                    <img src={'https://s2.coinmarketcap.com/static/img/coins/16x16/' + item.id + '.png'}></img>
+                    <td className= "item-name" > 
+                        <img className="img" src={'https://s2.coinmarketcap.com/static/img/coins/16x16/' + item.id + '.png'} alt="icon"></img>
                         {item.name}
                     </td>
-                    <td> 
-                        {item.quotes.BTC.market_cap}
+                    <td className= "item-market"> 
+                        {'$' + item.quotes.USD.market_cap.toLocaleString()}
                     </td>
-                    <td >
-                        {item.quotes.USD.price}
+                    <td className= "item-price" style={{color: "blue"}}>
+                        {'$' + item.quotes.USD.price.toFixed(2)}
                     </td>
-                    <td>
-                        {item.quotes.USD.percent_change_24h}
+                    <td className= "item-change" style={self.itemChange(item)}>
+                        {item.quotes.USD.percent_change_24h.toFixed(2) + '%'}
+                    </td>
+                    <td className= "item-graph">
+                        <img src={'https://s2.coinmarketcap.com/generated/sparklines/web/7d/usd/' + item.id + '.png'} alt="icon"></img>
                     </td>
                 </tr>
             )  
         })  
-        
+            
         return(
             <div className="container">
                 <div className="row">
-                <table className="table table-haver table-bordered"> 
+                <table className="table table-hover"> 
                     <thead >
                         <tr >
-                            <th scope="col">
+                            <th align="center">
                                 <h5>#</h5>
                             </th>
-                            <th onClick={self.ordenar.bind(self, items)} style={{cursor: 'pointer'}}>
-                                {console.log(items)}
+                            <th onClick={self.ordenarNombre.bind(self)} style={{cursor: 'pointer'}} align="center">
                                 <h5>Name</h5>
                             </th>                            
-                            <th scope="col">
+                            <th onClick={self.ordenarMarquet.bind(self)} style={{cursor: 'pointer'}}>
                                 <h5>Market cap</h5>
                             </th>
                             <th scope="col">
                                 <h5>Price</h5>
                             </th>
-                            <th scope="col">
+                            <th align="center">
                                 <h5>Change(24h)</h5>
+                            </th>
+                            <th scope="col">
+                                <h5>Price Graph (7d)</h5>
                             </th>
                         </tr>
                     </thead>                  
@@ -126,11 +169,11 @@ class BoxTarea extends Component {
                         </tbody>
                     </table>
                     </div>
-                <div> 
+                <div className="pag"> 
                     <Button bsStyle="link" onClick={self.previousPage.bind(self)}>
                         Pagina anterior
                     </Button>
-                    {'Pagina ' + (self.state.page+1) + ' de ' + self.state.totalCryptos/self.state.limit}
+                    {'Pagina ' + (self.state.page+1) + ' de ' + Math.round(self.state.totalCryptos/self.state.limit)}
                     <Button bsStyle="link" onClick={self.nextPage.bind(self)}>
                         Pagina siguiente
                     </Button>
